94a7bfadbaaf6eaedb96c5f27cde26e07dbeb5e6
kurniadikevin <kurniadikevin@gmail.com>
2022-06-17 19:49:56 +0700

    player name inut - updated

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 
 let board=[];
 
+// count game move    \
+let gameMove = 0;
+function move(){
+    
+    return function(){
+        gameMove++;
+        console.log('Move: '+gameMove);
+}}    
+const doMove = move();
+
+
+// for game result
+ function gameResult(a,b,c,cont,winner){
+    if(board[a] === board[b] && board[b]=== board[c] && board[a] === cont){
+     
+        console.log(winner);
+        //const winDec = document.createElement('div');
+        winDec.textContent = winner;
+        const dec = document.querySelector('.declaration');
+        dec.appendChild(winDec);
+        }; 
+}
 // input data for array board
 function onClick(val,input){
-    let player1 = document.querySelector('#player1').value;
-     let player2 = document.querySelector('#player2').value;
+    //let player1 = document.querySelector('#player1').value;
+    //let player2 = document.querySelector('#player2').value;
        board[val-1]= input;
        console.log(board);
        gameResult(0,1,2,'X', player1 + ' Win!');
@@ -28,24 +50,12 @@ function onClick(val,input){
   
        gameResult(0,4,8,'O',player2 + ' Win!');
        gameResult(2,4,6,'O',player2 + ' Win!');
-       
-    }
+       }
    
-// count game move    \
-let gameMove = 0;
-function move(){
-    
-    return function(){
-        gameMove++;
-        console.log('Move: '+gameMove);
-}}    
-const doMove = move();
-
 
 
 // event for every grid
 const makeFunction = function(num){
-    //const grid = document.querySelector(`.grid${num}`);
     const gridNew = document.createElement('div');
     gridNew.classList.add(`.grid${num}`);
     let container = document.querySelector('.container');
@@ -53,7 +63,6 @@ const makeFunction = function(num){
 
     gridNew.addEventListener('click',function(){
         doMove();
-        
         if ( gameMove % 2 === 0){
             this.textContent='O';
             onClick(num,'O');
@@ -61,57 +70,42 @@ const makeFunction = function(num){
             console.log('Draw Game!');
             this.textContent='X';
             onClick(num,'X');
-            
             if( winDec.textContent == '' ){
                 const winDec = document.createElement('div');
                 winDec.textContent = 'Draw Game!';
                 const dec = document.querySelector('.declaration');
                 dec.appendChild(winDec);
             }
-        } 
-        else{
+        }else{
             this.textContent='X';
             onClick(num,'X');
-        }
-            
-    },{once:true});
+        }},{once:true});
 }
+
+let player11;
+let player2;
+
 //start game function
-
-
 const startGame = function(){
-    
+      player1 = document.querySelector('#player1').value;
+      player2 = document.querySelector('#player2').value;
     for(let i=0; i<9; i++){
         makeFunction(i+1);}
         console.log('clicked');
-     let inputName =  document.querySelector('.input-name');
-     
-    };
-    
+        let inputName = document.querySelector('.input-name');
+        inputName.innerHTML='';
+     };
+ 
+// Dom for starting game     
 const startButton = document.querySelector('.start-game');
 startButton.addEventListener('click',startGame,{once: true});
-
-
-
-   console.log(board[0]);
+     console.log(board[0]);
    const winDec = document.createElement('div');
+   console.log(winDec.textContent);//test
 
-gameResult = function(a,b,c,cont,winner){
-    if(board[a] === board[b] && board[b]=== board[c] && board[a] === cont){
-        console.log(winner);
-        //const winDec = document.createElement('div');
-        winDec.textContent = winner;
-        const dec = document.querySelector('.declaration');
-        dec.appendChild(winDec);
-        
-    }; 
 
-}
-
-console.log(winDec.textContent);
-
-// reload
+// reload game
 let reload = document.querySelector('.reload');
 reload.addEventListener('click',function(){
     location.reload();
-})
+    })
